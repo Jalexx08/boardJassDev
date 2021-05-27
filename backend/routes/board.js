dfa1029d63c6bf67056b77119b51048c2c078e2f
Jalexss <jhalx2198@gmail.com>
2021-05-27 16:06:14 -0500

    Add update and delete user's activities

--- backend/routes/board.js
+++ backend/routes/board.js
@@ -39,3 +39,33 @@ router.get("/listTask", Auth, async (req, res) => {
 });
 
 module.exports = router;
+
+//* Update  user's activities
+
+router.put("/updateTask", Auth, async (req, res) => {
+	const user = await User.findById(req.user._id);
+	if (!user) return res.status(401).send("Usuario no existe");
+
+	const board = await Board.findByIdAndUpdate(req.body._id, {
+        userId: user._id,
+        name: req.body.name,
+        description:req.body.description,
+        status:req.body.status,
+    });
+	if (!board)
+		return res.status(401).send("No es posible editar la información");
+
+	return res.status(200).send({ board });
+});
+
+//*  Delete  user's activities
+
+router.delete("/:_id", Auth, async (req, res) => {
+	const user = await User.findById(req.user._id);
+	if (!user) return res.status(401).send("Usuario no existe");
+
+	const board = await Board.findByIdAndDelete(req.params._id);
+	if (!board) return res.status(401).send(" No se encontró ninguna tarea para eliminar");
+
+	return res.status(200).send("Se ha eliminado la actividad");
+});
